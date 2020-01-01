8e58b92c4b4d75507e61c4c8c8ee1564ba1234ff
Fezisa Rwayi <Fezisa.r@gmail.com>
2020-01-01 15:17:55 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,52 +1,58 @@
 //import React from 'react';
-import React, {Component} from 'react' ;
-import TodoInput from './components/TodoInput' ;
-import TodoList from './components/TodoList' ;
+import React, { Component } from 'react';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
 
-//import 'bootstrap/dist/css/bootstrap.min.css' ;
-//import uuid from 'uuid' ; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import uuid from 'uuid';
 
 
 class App extends Component {
-  
+
   render() {
-    return (
-    <div> 
-    hello from the main app
-    <TodoInput />
-    <TodoList />
-    </div>
+    return ( 
+    
+       <div className="container">
+        <div className="row">
+          <div className="col-18 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">ToDo Input</h3>
+
+            <TodoInput />
+            <TodoList />
+          </div>
+        </div> 
+      </div>
     );
   }
 }
-
+  
 export default App;
 
 
 
 
-//import React from 'react';
-//import './App.css';
+      //import React from 'react';
+      //import './App.css';
 
 /* function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
         </a>
-      </header>
-    </div>
-  );
-} */
+            </header>
+          </div>
+          );
+        } */
 
 // export default App;
